77582f63d55a2ea8c4652908af1cdb6d9e3970b8
reda baskiki <baskikireda.rb@gmail.com>
2025-01-22 13:50:22 +0100

    first commit

---
+++ src/assets/index.ts
@@ -0,0 +1,51 @@
+import ship from './Ship.png';
+import logo from './Logo.png';
+import search from './search.png';
+import footer from './Footer.png';
+import fb from './fb.png';
+import ig from './ig.png';
+import x from './x.png';
+import pt from './pt.png';
+import shop from "./shopping-cart.png"
+import F89 from "./F89.png";
+import F90 from "./F90.png";
+import F91 from "./F91.png";
+import hero from "./Hero.png";
+import banner1 from "./banner 1.png";
+import banner2 from "./banner 2.png";
+import C42 from "./C42.png";
+import C43 from "./C43.png";
+import C49 from "./C49.png";
+import C50 from "./C50.png";
+import C51 from "./C51.png";
+import shoot1 from "./Rectangle 23.png";
+import shoot2 from "./Rectangle 24.png";
+import shoot3 from "./Rectangle 25.png";
+import shoot4 from "./Rectangle 26.png";
+import rev1 from "./rev1.png";
+import rev2 from "./rev2.png";
+import rev3 from "./rev3.png";
+import back from "./BackTo.png";
+import next from "./NextTo.png";
+import try1 from "./try1.png";
+import like from "./Heart.png";
+import emptyStar from "./Vector.png";
+import star from "./Star.png"
+import pop1 from "./pop1.png"
+import plus from "./Primary fill.png";
+import contact1 from "./contact/Frame 220.png";
+import contact2 from "./contact/Frame 221.png";
+import contact3 from "./contact/Frame 222.png";
+import  detail1 from "./detail/detail1.png";
+import  detail2 from "./detail/detail2.png";
+import  detail3 from "./detail/detail3.png";
+import maps from "./contact/maps.png";
+import usacheck from "./check out/American Express.png";
+import discover from "./check out/Discover Card.png";
+import MasterCard from "./check out/MasterCard.png";
+import union from "./check out/UnionPay.png";
+import  visa  from "./check out/visa.png";
+import shopp from "./ShoppingCart.svg"
+export  {shopp, usacheck, discover, union , MasterCard, visa
+    ,  detail1,detail2,detail3, maps,contact1 , contact2 , contact3, pop1, plus, star, emptyStar, like, back
+    , next ,rev1, rev2,rev3, ship, logo, shop, search,footer , banner1 , banner2,fb , ig ,x, pt, F89, F90, F91 ,hero,C42 , C43, C49, C50, C51 , shoot1, shoot2, shoot3, shoot4 ,try1}
